5a9f7c58e3315c1bd29fb9ec87835188a6c7d83a
NETSTALK0000 <rudenkolesa84@gmail.com>
2026-02-24 03:33:41 +0200

    Fix indentation in _get_client method

--- legacy/web/root.py
+++ legacy/web/root.py
@@ -278,8 +278,8 @@ class Web:
         return web.Response(status=201, body=self._qr_login.url)
 
     def _get_client(self) -> CustomTelegramClient:
-    session = MemorySession()
-    session.set_dc(2, "149.154.167.51", 443)
+        session = MemorySession()
+        session.set_dc(2, "149.154.167.51", 443)
 
     return CustomTelegramClient(  
     session,  
