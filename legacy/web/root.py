61d604f5c1f7e9cd58156f3e029cecf514f08f60
NETSTALK0000 <rudenkolesa84@gmail.com>
2026-02-24 03:26:48 +0200

    Fix formatting and improve _get_client method

--- legacy/web/root.py
+++ legacy/web/root.py
@@ -277,11 +277,12 @@ class Web:
 
         return web.Response(status=201, body=self._qr_login.url)
 
-    def _get_client(self) -> CustomTelegramClient:
-session = MemorySession()
+    def _get_client(self) -> 
+    CustomTelegramClient:
+    session = MemorySession()
 session.set_dc(2, "149.154.167.51", 443)
 
-return CustomTelegramClient(  
+    return CustomTelegramClient(  
     session,  
     self.api_token.ID,  
     self.api_token.HASH,  
